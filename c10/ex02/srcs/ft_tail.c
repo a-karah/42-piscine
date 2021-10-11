03855f1a4f3ccd548f9a3b6da97964786a0a9393
a-karah <akarahan096@gmail.com>
2021-10-11 20:59:38 +0300

    c10-ex02 add reading from end

--- c10/ex02/srcs/ft_tail.c
+++ c10/ex02/srcs/ft_tail.c
@@ -6,7 +6,7 @@
 /*   By: akarahan <akarahan@student.42istanbul      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 14:22:24 by akarahan          #+#    #+#             */
-/*   Updated: 2021/10/07 14:22:25 by akarahan         ###   ########.fr       */
+/*   Updated: 2021/10/11 20:46:53 by akarahan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,77 @@ void	read_stdin(void)
 	}
 }
 
-void	read_file(char *fname, int offset)
+int	total_bytes(char *fname)
+{
+	int	fd;
+	int	readsize;
+	int	total_read;
+	char buf[BIG_BUFFER_SIZE];
+
+	fd = open(fname, O_RDONLY);
+	if (fd < 1)
+		handle_error(fname);
+	else
+	{
+		total_read = 0;
+		while (1)
+		{
+			readsize = read(fd, buf, sizeof(buf));
+			if (readsize < 0)
+			{
+				handle_error(fname);
+				break ;
+			}
+			else if (readsize == 0)
+				break ;
+			total_read += readsize;
+		}
+	}
+	return (total_read);
+}
+
+void	read_from_end(char *fname, int offset)
+{
+	int		fd;
+	char	buf[BUFFER_SIZE];
+	int		readsize;
+	int		total_read;
+	int		total_size;
+	int		offset_from_beginning;
+
+	fd = open(fname, O_RDONLY);
+	if (fd < 1)
+		handle_error(fname);
+	else
+	{
+		total_read = 0;
+		total_size = total_bytes(fname);
+		offset_from_beginning = total_size - offset;
+		while (1)
+		{
+			readsize = read(fd, buf, sizeof(buf));
+			if (readsize < 0)
+			{
+				handle_error(fname);
+				break ;
+			}
+			else if (readsize == 0)
+				break ;
+			if (total_read < offset_from_beginning)
+			{
+				if (offset_from_beginning < total_read + readsize)
+					write(STDOUT, (buf + offset_from_beginning % BUFFER_SIZE),
+							readsize - offset_from_beginning % BUFFER_SIZE);
+			}
+			else
+				write(STDOUT, buf, readsize);
+			total_read += readsize;
+		}
+		close(fd);
+	}
+}
+
+void	read_from_beginning(char *fname, int offset)
 {
 	int		fd;
 	char	buf[BUFFER_SIZE];
@@ -57,7 +127,8 @@ void	read_file(char *fname, int offset)
 			if (total_read < offset)
 			{
 				if (offset < total_read + readsize)
-					write(STDOUT, (buf + offset % BUFFER_SIZE), readsize - offset % BUFFER_SIZE);
+					write(STDOUT, (buf + offset % BUFFER_SIZE),
+						readsize - offset % BUFFER_SIZE);
 			}
 			else
 				write(STDOUT, buf, readsize);
